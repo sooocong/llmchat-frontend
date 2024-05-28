e724214349d83b9b6471f0b8f619cf5e27253105
who0803 <10rlatkdtn@naver.com>
2024-05-29 01:46:42 +0900

    feat: Create useHideByClickOutside custom hook
    - useHideByClickOutside : 모달 외의 부분 클릭 시, 모달 닫는 커스텀 훅
    	- 매개변수
    		- onClose: 모달 닫는 콜백함수
    		- targetName: 클릭 시, 닫히지 않을 예외 부분의 클래스
    		  이름
    	- return
    		- 모달의 ref에 적용할 ref

---
+++ src/hooks/useHideByClickOutside.ts
@@ -0,0 +1,27 @@
+import { useEffect, useRef } from 'react';
+
+const useHideByClickOutside = (onClose: () => void, targetName: string) => {
+  const modalRef = useRef<HTMLUListElement>(null);
+
+  useEffect(() => {
+    function handleClickOutside(event: MouseEvent) {
+      const target = event.target as HTMLElement;
+      if (
+        modalRef.current &&
+        !modalRef.current.contains(target) &&
+        modalRef.current.parentNode !== target.closest(targetName)
+      ) {
+        onClose();
+      }
+    }
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
+  return modalRef;
+};
+
+export { useHideByClickOutside };
