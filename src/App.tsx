3a26d5e75ab2535eeea090ddd24a6ec2d0b94c80
cozy01 <yuji10085@gamil.com>
2024-06-02 05:36:38 +0900

    feat: Add MyPage component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import NavigationTabs from './components/MyPage/NavigationTabs';
+import MyProfilePRovider from './provider/myProfileProvider';
+import MyProfileProvider from './provider/myProfileProvider';
+import MyPageView from './components/MyPage/MyPageView';
+import MyPageProfile from './components/MyPage/MyPageProfile';
+import PopupProvider from './provider/popupProvider';
 
 
 function App() {
   return (
-    <div className="App">
-        <NavigationTabs />
+    <div className="App flex justify-content-center">
+      <MyProfileProvider>
+        <PopupProvider>
+          <div className={'w-full w-m-1440'}>
+            <MyPageProfile />
+            <NavigationTabs />
+            <MyPageView />
+          </div>
+        </PopupProvider>
+      </MyProfileProvider>
     </div>
   );
 }
