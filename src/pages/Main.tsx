8863b091f269488f1e9fb066d403c376b9e24797
who0803 <10rlatkdtn@naver.com>
2024-06-05 15:33:44 +0900

    feat: Add key to Chatcoponent
    - Chatcomponent에 키 추가: 스레드id가 변경됐을 때, 챗 컴포넌트를 새로
      생성하기 위해서 추가

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { SidebarMenu } from '../components/Sidebar';
 import ChatComponent from '../components/Chat/ChatComponent';
+import { useThreads } from '../hooks';
 
 function Main() {
+  const {selectedThreadId} = useThreads()
   return (
     <div style={{ display: 'flex', height: '100vh' }}>
       <SidebarMenu />
-      <ChatComponent />
+      <ChatComponent key={selectedThreadId}/>
     </div>
   );
 }
