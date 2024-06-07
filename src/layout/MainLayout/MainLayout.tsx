113ac068068c67ba5a4617c69088364ca1508566
who0803 <10rlatkdtn@naver.com>
2024-06-07 19:31:31 +0900

    feat: Create Main layout
    - 사이드바와 헤더를 갖는 레이아웃 구현

---
+++ src/layout/MainLayout/MainLayout.tsx
@@ -0,0 +1,67 @@
+import React, { useEffect, useState, ReactNode } from 'react';
+import { Chat, SidebarMenu, Header } from '../../components';
+import style from './MainLayout.module.css';
+import { getIsSidebarOpen, setIsSidebarOpen } from '../../utils';
+
+const MainLayout = ({ children }: { children: ReactNode }) => {
+  const mediaQuery = window.matchMedia('(max-width: 768px)');
+  const initIsSidebarVisible = mediaQuery.matches
+    ? !mediaQuery.matches
+    : getIsSidebarOpen() === null
+      ? true
+      : getIsSidebarOpen();
+  const [isSidebarVisible, setIsSidebarVisible] =
+    useState(initIsSidebarVisible);
+  const [isOverlayed, setIsOverlayed] = useState(mediaQuery.matches);
+
+  const toggleSidebar = () => {
+    setIsSidebarVisible(!isSidebarVisible);
+    setIsSidebarOpen(!isSidebarVisible);
+    if (mediaQuery.matches) {
+      setIsOverlayed(true);
+    }
+  };
+
+  useEffect(() => {
+    const handleResize = (event: MediaQueryListEvent) => {
+      if (event.matches) {
+        setIsSidebarVisible(false);
+      } else {
+        setIsSidebarVisible(getIsSidebarOpen());
+        setIsOverlayed(false);
+      }
+    };
+
+    if (getIsSidebarOpen() === null) {
+      setIsSidebarOpen(true);
+    }
+    mediaQuery.addEventListener('change', handleResize);
+
+    return () => {
+      mediaQuery.removeEventListener('change', handleResize);
+    };
+  }, []);
+
+  return (
+    <div className={style.container}>
+      <SidebarMenu
+        isSidebarVisible={isSidebarVisible}
+        onClose={toggleSidebar}
+      />
+      <Header
+        isSidebarVisible={isSidebarVisible}
+        onOpenSidebar={toggleSidebar}
+      />
+      <div
+        className={`${style.childrenContainer} ${isSidebarVisible ? style.sidebarVisible : style.sidebarHidden}`}
+      >
+        {children}
+      </div>
+      {isSidebarVisible && isOverlayed && (
+        <div className={style.overlay} onClick={toggleSidebar}></div>
+      )}
+    </div>
+  );
+};
+
+export { MainLayout };
