2b98ff4bf6295709ad7a73197f90e156246fcd67
who0803 <10rlatkdtn@naver.com>
2024-06-07 19:39:03 +0900

    feat: Implement routing
    - 초기 라우팅 구현

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles/index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { MainPage, LoginPage, MyPage, SignupPage } from './pages';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      { index: true, element: <MainPage /> },
+      {
+        path: 'login',
+        element: <LoginPage />,
+      },
+      {
+        path: 'signup',
+        element: <SignupPage />,
+      },
+      {
+        path: 'mypage',
+        element: <MyPage />,
+      },
+    ],
+  },
+]);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render(
-  // <React.StrictMode>
-    <App />
-  // </React.StrictMode>
-);
+
+root.render(<RouterProvider router={router} />);
