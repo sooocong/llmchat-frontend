6d985955f4b7a8d756208a2d304090eb1777cab1
who0803 <10rlatkdtn@naver.com>
2024-06-09 15:14:42 +0900

    feat: Add routing to CreateButton
    - 새쓰레드 만들기 버튼에 라우팅 기능 추가

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,6 +4,7 @@ import { ReactComponent as AerochatLogo } from '../../assets/aerochatLogo.svg';
 import { ReactComponent as RightIcon } from '../../assets/right-arrow-button.svg';
 import { ReactComponent as CreateIcon } from '../../assets/create-button.svg';
 import { useThreads } from '../../hooks';
+import { useNavigate } from 'react-router-dom';
 
 interface IHeader {
   isSidebarVisible: boolean;
@@ -11,6 +12,12 @@ interface IHeader {
 }
 const Header = ({ isSidebarVisible, onOpenSidebar }: IHeader) => {
   const { initChatting } = useThreads();
+  const navigation = useNavigate();
+
+  const handleCreateClick = () => {
+    initChatting();
+    navigation('/');
+  };
 
   return (
     <header
@@ -19,7 +26,7 @@ const Header = ({ isSidebarVisible, onOpenSidebar }: IHeader) => {
       <button className={style.IconButton} onClick={onOpenSidebar}>
         <RightIcon />
       </button>
-      <button className={style.IconButton} onClick={initChatting}>
+      <button className={style.IconButton} onClick={handleCreateClick}>
         <CreateIcon />
       </button>
       <AerochatLogo />
